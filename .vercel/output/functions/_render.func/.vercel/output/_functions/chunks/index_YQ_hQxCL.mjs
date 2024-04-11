6a976cf3443a891350c3d52af003a77fe4cd2eb8
Chloe Arciniega <chloe@sapphic.moe>
2024-04-11 06:42:27 +0500

    chore: oops, delete .vercel directory

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/index_YQ_hQxCL.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./prerender_B_1v0X0D.mjs');
-
-export { page };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/verify_C6HF5agT.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/verify_CLKEO8w7.mjs');
-
-export { page };
